8ab21762b61b2343e2a21644b947d76301ab9b5a
hainguyen2k <ngxuanhai1000@gmail.com>
2021-03-24 21:30:04 +0700

    BaiTapTuan2

---
+++ 18020331_NguyenXuanHai/app/src/main/java/com/example/a18020331_nguyenxuanhai/MainActivity3.java
@@ -0,0 +1,14 @@
+package com.example.a18020331_nguyenxuanhai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
